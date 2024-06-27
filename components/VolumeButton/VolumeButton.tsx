ef0f389a71e5869d9cecdea7e73da0603f46df4d
Tormod Aase <tormod.aase@bouvet.no>
2024-06-27 15:30:02 +0200

    Added haptics

--- components/VolumeButton/VolumeButton.tsx
+++ components/VolumeButton/VolumeButton.tsx
@@ -1,13 +1,12 @@
-import { Button, EDSStyleSheet, PressableHighlight, Typography, useStyles } from "@equinor/mad-components";
-import { Pressable, View } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
-import { SvgXml } from "react-native-svg";
-import { Variant } from "./types";
-import { VolumeSymbol } from "./VolumeSymbol";
+import { EDSStyleSheet, useStyles } from "@equinor/mad-components";
 import { colors } from "@equinor/mad-components/dist/styling";
+import * as Haptics from "expo-haptics";
+import { View } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { VolumeSymbol } from "./VolumeSymbol";
+import { Variant } from "./types";
 
 const HEIGHT = 214;
-
 
 export type VolumeButtonProps<V extends Variant> = {
   variant: V;
@@ -18,9 +17,13 @@ export const VolumeButton = <V extends Variant>({
   onPress,
 }: VolumeButtonProps<V>) => {
   const styles = useStyles(volumeButtonStyles, variant);
+  const haptics = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+  };
   return (
-    <View style={{ height: HEIGHT / 2, overflow: "hidden" }}>
+    <View style={styles.container}>
       <TouchableHighlight
+        onPressIn={haptics}
         onPress={() => onPress(variant)}
         style={styles.pressable}
         underlayColor={colors.interactive_primary_light_hover}
@@ -35,22 +38,23 @@ export const VolumeButton = <V extends Variant>({
 };
 
 const volumeButtonStyles = EDSStyleSheet.create((theme, variant: Variant) => {
-    const marginTop = variant === "-" ? -HEIGHT/2 : 0
-    const symbolContainerMarginTop = variant === "-" ? "50%" : undefined
+  const marginTop = variant === "-" ? -HEIGHT / 2 : 0;
+  const symbolContainerMarginTop = variant === "-" ? "50%" : undefined;
 
-    return {
-        pressable: {
-            height: 214,
-            width: 214,
-            backgroundColor: theme.colors.interactive.primary,
-            borderRadius: 180,
-            alignItems: "center",
-            marginTop 
-        },
-        symbolContainer: {
-          height: "50%",
-          justifyContent: "center",
-          marginTop: symbolContainerMarginTop
-        }
-    }
-})
+  return {
+    container: { height: HEIGHT / 2, overflow: "hidden" },
+    pressable: {
+      height: 214,
+      width: 214,
+      backgroundColor: theme.colors.interactive.primary,
+      borderRadius: 180,
+      alignItems: "center",
+      marginTop,
+    },
+    symbolContainer: {
+      height: "50%",
+      justifyContent: "center",
+      marginTop: symbolContainerMarginTop,
+    },
+  };
+});
