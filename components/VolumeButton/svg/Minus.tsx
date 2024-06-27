8241bee6dbf48f9820883fa2f40e1a99a45d4749
Tormod Aase <tormod.aase@bouvet.no>
2024-06-27 15:37:19 +0200

    Update color in svg components

--- components/VolumeButton/svg/Minus.tsx
+++ components/VolumeButton/svg/Minus.tsx
@@ -1,12 +1,14 @@
-import * as React from "react"
+import { useToken } from "@equinor/mad-components"
 import Svg, { SvgProps, Path } from "react-native-svg"
-export const MinusSvg = (props: SvgProps) => (
-  <Svg
+export const MinusSvg = (props: SvgProps) => {
+    const token = useToken()
+    const color = token.colors.text.primaryInverted;
+    return <Svg
     width={20}
     height={4}
     fill="none"
     {...props}
-  >
-    <Path fill="#fff" d="M0 0h20v4H0z" />
-  </Svg>
-)
+    >
+    <Path fill={color} d="M0 0h20v4H0z" />
+    </Svg>
+}
--- components/VolumeButton/svg/Plus.tsx
+++ components/VolumeButton/svg/Plus.tsx
@@ -1,13 +1,16 @@
-import * as React from "react"
+import { useToken } from "@equinor/mad-components";
 import Svg, { SvgProps, Path } from "react-native-svg"
-export const PlusSvg = (props: SvgProps) => (
+export const PlusSvg = (props: SvgProps) => {
+    const token = useToken()
+    const color = token.colors.text.primaryInverted;
+    return (
   <Svg
     width={20}
     height={20}
     fill="none"
     {...props}
   >
-    <Path fill="#fff" d="M0 8h20v4H0z" />
-    <Path fill="#fff" d="M8 20V0h4v20z" />
+    <Path fill={color} d="M0 8h20v4H0z" />
+    <Path fill={color} d="M8 20V0h4v20z" />
   </Svg>
-)
+)}
