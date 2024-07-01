a3adb370ed994b1d1c14e209f90c7bdda7547480
Tormod Aase <tormod.aase@bouvet.no>
2024-07-01 14:20:33 +0200

    feat: SoundButton (mix of play button and volume buttons) (#23)

--- screens/HelloWorld.tsx
+++ screens/HelloWorld.tsx
@@ -1,27 +1,46 @@
-import { Button, EDSStyleSheet, Typography, useStyles } from "@equinor/mad-components";
+import {
+  Button,
+  EDSStyleSheet,
+  Typography,
+  useStyles,
+} from "@equinor/mad-components";
+import { useRef } from "react";
 import { View } from "react-native";
-import { VolumeButton } from "../components/VolumeButton/VolumeButton";
+import {
+  SoundButton,
+  SoundButtonProps,
+} from "../components/VolumeButton/SoundButton";
+import { SoundButtonControls } from "../components/VolumeButton/types";
 import { useDictionary } from "../language";
 import { useAttenuationAppNavigation } from "../navigation/useAttenuationAppNavigation";
 
 export const HelloWorld = () => {
-    const styles = useStyles(themeStyles);
-    const { navigate } =useAttenuationAppNavigation()
-    const dictionary = useDictionary();
-    return <View style={styles.container}>
-        <VolumeButton variant="+" onPress={(v) => undefined} />
-        <View style={{height:16}} />
-        <VolumeButton variant="-" onPress={(v) => undefined} />
-        <Typography>{dictionary.helloWorld}</Typography>
-        <Button title="Start testen" onPress={() => navigate("TestScreen")}/>
+  const styles = useStyles(themeStyles);
+  const { navigate } = useAttenuationAppNavigation();
+  const dictionary = useDictionary();
+  const soundButtonRef = useRef<SoundButtonControls>(null);
+  const onPress: SoundButtonProps["onPress"] = (e) => {
+    if (e.type === "play") {
+      soundButtonRef.current?.setSoundButtonType("volume");
+    } else {
+      soundButtonRef.current?.setSoundButtonType("play");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Typography>{dictionary.helloWorld}</Typography>
+      <SoundButton ref={soundButtonRef} onPress={onPress} />
+      <Button title="Start testen" onPress={() => navigate("TestScreen")} />
     </View>
-}
+  );
+};
 
 const themeStyles = EDSStyleSheet.create((theme) => ({
-    container: {
-        flex: 1,
-        backgroundColor: theme.colors.container.background,
-        alignItems: 'center',
-        justifyContent: "center"
-    }
-}))
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.container.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
